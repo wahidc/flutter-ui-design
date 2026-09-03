7e0a3d86f887879412df917845a07e0797d461a8
Wahid Chowdhury <wahidchowdhury2007@gmail.com>
2026-09-03 10:50:28 +0600

    Cart Bottom NavBar Added

--- 01_food_app/lib/Widget/CartBottomNavBar.dart
+++ 01_food_app/lib/Widget/CartBottomNavBar.dart
@@ -3,6 +3,53 @@ import 'package:flutter/material.dart';
 class CartBottomNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar();
+    return BottomAppBar(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Total",
+                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 15),
+                Text(
+                  "\$80",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.red),
+                padding: WidgetStateProperty.all(
+                  EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  )
+                )
+              ),
+              child: Text(
+                "Order Now",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
